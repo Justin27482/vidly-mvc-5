999818a05c0523866f4829e79188843819b5b0e0
Justin Verthé <jverthe@nopec.org>
2023-04-27 15:03:58 -0400

    Section 2 Exercise 1

---
+++ Vidly/Migrations/202304271855311_AddNameToMembershipType.cs
@@ -0,0 +1,24 @@
+﻿namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddNameToMembershipType : DbMigration
+    {
+        public override void Up()
+        {
+            
+            Sql("UPDATE MembershipTypes SET DurationInMonths = 3 WHERE id = 3"); //Fixing a Data error in DB created in last Migration
+            AddColumn("dbo.MembershipTypes", "Name", c => c.String(nullable: false, maxLength: 30));
+            Sql("UPDATE MembershipTypes SET Name = 'Pay As You Go' WHERE id = 1");
+            Sql("UPDATE MembershipTypes SET Name = 'Monthly' WHERE id = 2");
+            Sql("UPDATE MembershipTypes SET Name = 'Quarterly' WHERE id = 3");
+            Sql("UPDATE MembershipTypes SET Name = 'Yearly' WHERE id = 4");
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.MembershipTypes", "Name");
+        }
+    }
+}
