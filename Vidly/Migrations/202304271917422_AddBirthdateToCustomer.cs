8c3f4b522980de66a6dd20ea572cd86f224877ed
Justin Verthé <jverthe@nopec.org>
2023-04-27 21:26:17 -0400

    Middle of Exercise 2

---
+++ Vidly/Migrations/202304271917422_AddBirthdateToCustomer.cs
@@ -0,0 +1,22 @@
+﻿namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddBirthdateToCustomer : DbMigration
+    {
+        public override void Up()
+        {
+            DropIndex("dbo.Customers", new[] { "MembershipTypeID" });
+            AddColumn("dbo.Customers", "Birthdate", c => c.DateTime());
+            CreateIndex("dbo.Customers", "MembershipTypeId");
+        }
+        
+        public override void Down()
+        {
+            DropIndex("dbo.Customers", new[] { "MembershipTypeId" });
+            DropColumn("dbo.Customers", "Birthdate");
+            CreateIndex("dbo.Customers", "MembershipTypeID");
+        }
+    }
+}
---
+++ Vidly/Migrations/202304271949401_AddMoviesToModel.cs
@@ -0,0 +1,47 @@
+﻿namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddMoviesToModel : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Movies",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false),
+                        ReleaseDate = c.DateTime(nullable: false),
+                        DateAddedToDB = c.DateTime(nullable: false),
+                        NumberInStock = c.Int(nullable: false),
+                        MovieGenre_Id = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.MovieGenres", t => t.MovieGenre_Id, cascadeDelete: true)
+                .Index(t => t.MovieGenre_Id);
+            
+            CreateTable(
+                "dbo.MovieGenres",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 255),
+                    })
+                .PrimaryKey(t => t.Id);
+
+            //FINISH INSERTING GENRES HERE!!!!!!!!!
+            Sql("INSERT INTO MovieGenres (Id, N");
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Movies", "MovieGenre_Id", "dbo.MovieGenres");
+            DropIndex("dbo.Movies", new[] { "MovieGenre_Id" });
+            DropTable("dbo.MovieGenres");
+            DropTable("dbo.Movies");
+        }
+    }
+}
